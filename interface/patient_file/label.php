18c8d6db413aada55be8a1d2eeae82e842a3ec6e
teryhill <teryhill@yahoo.com>
2015-03-10 17:55:47 -0400

    rebasing

--- interface/patient_file/label.php
+++ interface/patient_file/label.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 /** 
  * interface/patient_file/label.php Displaying a PDF file of Labels for printing. 
  * 
@@ -33,45 +34,91 @@ require_once("$srcdir/formatting.inc.php");
 
 //Get the data to place on labels
 //
+=======
+/* Copyright (C) 2014 Terry Hill <terry@lillysystems.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+// I used the program example supplied with the Avery Label Print Class to produce this program
+//
+
+include_once("../globals.php");
+require_once('PDF_Label.php');
+
+//Get the data to place on labels
+//
+
+>>>>>>> Files to allow Chart labels
 $patdata = sqlQuery("SELECT " .
   "p.fname, p.mname, p.lname, p.pubpid, p.DOB, " .
   "p.street, p.city, p.state, p.postal_code, p.pid " .
   "FROM patient_data AS p " .
+<<<<<<< HEAD
   "WHERE p.pid = ? LIMIT 1", array($pid));
+=======
+  "WHERE p.pid = '$pid' LIMIT 1");
+>>>>>>> Files to allow Chart labels
 
 // re-order the dates
 //
   
+<<<<<<< HEAD
 $today = oeFormatShortDate($date='today');
 $dob = oeFormatShortDate($patdata['DOB']);
+=======
+$today = date('m/d/Y');
+$dob   = substr($patdata['DOB'],5,2) ."/". Substr($patdata['DOB'],8,2) ."/". Substr($patdata['DOB'],0,4);
+>>>>>>> Files to allow Chart labels
 
 //get label type and number of labels on sheet
 //
 
+<<<<<<< HEAD
 if ($GLOBALS['chart_label_type'] == '1') { 
+=======
+if ($GLOBALS['label_type'] == '1') { 
+>>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5160');
 $last = 30;
 }
 
+<<<<<<< HEAD
 if ($GLOBALS['chart_label_type'] == '2') { 
+=======
+if ($GLOBALS['label_type'] == '2') { 
+>>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5161');
 $last = 20;
 }
 
+<<<<<<< HEAD
 if ($GLOBALS['chart_label_type'] == '3') { 
+=======
+if ($GLOBALS['label_type'] == '3') { 
+>>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5162');
 $last = 14;
 }
 
 $pdf->AddPage();
 
+<<<<<<< HEAD
 // Added spaces to the sprintf for Fire Fox it was having a problem with alignment 
 $text = sprintf("  %s %s\n  %s\n  %s\n  %s", $patdata['fname'], $patdata['lname'], $dob, $today, $patdata['pid']);
 
+=======
+>>>>>>> Files to allow Chart labels
 // For loop for printing the labels 
 // 
 
 for($i=1;$i<=$last;$i++) {
+<<<<<<< HEAD
+=======
+	$text = sprintf("%s %s\n%s\n%s\n%s", $patdata['fname'], $patdata['lname'], $dob, $today, $patdata['pid']);
+>>>>>>> Files to allow Chart labels
 	$pdf->Add_Label($text);
 }
 
