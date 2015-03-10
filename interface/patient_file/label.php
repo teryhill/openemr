4f4b629cdf4f1418d7d646bb34d6db0fc29ff7f6
teryhill <teryhill@yahoo.com>
2015-03-10 18:16:57 -0400

    fixed conflicts

--- interface/patient_file/label.php
+++ interface/patient_file/label.php
@@ -34,7 +34,6 @@ require_once("$srcdir/formatting.inc.php");
 
 //Get the data to place on labels
 //
-=======
 /* Copyright (C) 2014 Terry Hill <terry@lillysystems.com>
  *
  * This program is free software; you can redistribute it and/or
@@ -51,74 +50,50 @@ require_once('PDF_Label.php');
 //Get the data to place on labels
 //
 
->>>>>>> Files to allow Chart labels
 $patdata = sqlQuery("SELECT " .
   "p.fname, p.mname, p.lname, p.pubpid, p.DOB, " .
   "p.street, p.city, p.state, p.postal_code, p.pid " .
   "FROM patient_data AS p " .
-<<<<<<< HEAD
   "WHERE p.pid = ? LIMIT 1", array($pid));
-=======
-  "WHERE p.pid = '$pid' LIMIT 1");
->>>>>>> Files to allow Chart labels
-
 // re-order the dates
 //
   
-<<<<<<< HEAD
 $today = oeFormatShortDate($date='today');
 $dob = oeFormatShortDate($patdata['DOB']);
-=======
-$today = date('m/d/Y');
-$dob   = substr($patdata['DOB'],5,2) ."/". Substr($patdata['DOB'],8,2) ."/". Substr($patdata['DOB'],0,4);
->>>>>>> Files to allow Chart labels
+
 
 //get label type and number of labels on sheet
 //
 
-<<<<<<< HEAD
+
 if ($GLOBALS['chart_label_type'] == '1') { 
-=======
-if ($GLOBALS['label_type'] == '1') { 
->>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5160');
 $last = 30;
 }
 
-<<<<<<< HEAD
+
 if ($GLOBALS['chart_label_type'] == '2') { 
-=======
-if ($GLOBALS['label_type'] == '2') { 
->>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5161');
 $last = 20;
 }
 
-<<<<<<< HEAD
+
 if ($GLOBALS['chart_label_type'] == '3') { 
-=======
-if ($GLOBALS['label_type'] == '3') { 
->>>>>>> Files to allow Chart labels
 $pdf = new PDF_Label('5162');
 $last = 14;
 }
 
 $pdf->AddPage();
 
-<<<<<<< HEAD
+
 // Added spaces to the sprintf for Fire Fox it was having a problem with alignment 
 $text = sprintf("  %s %s\n  %s\n  %s\n  %s", $patdata['fname'], $patdata['lname'], $dob, $today, $patdata['pid']);
 
-=======
->>>>>>> Files to allow Chart labels
+
 // For loop for printing the labels 
 // 
 
 for($i=1;$i<=$last;$i++) {
-<<<<<<< HEAD
-=======
-	$text = sprintf("%s %s\n%s\n%s\n%s", $patdata['fname'], $patdata['lname'], $dob, $today, $patdata['pid']);
->>>>>>> Files to allow Chart labels
 	$pdf->Add_Label($text);
 }
 
