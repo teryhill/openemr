f50d0300ebe62eead1d870c034c1ed83f268ada8
teryhill <teryhill@yahoo.com>
2015-09-05 10:38:01 -0400

    Sherwin's request for iframe changes
    Sherwin's request for iframe changes

--- interface/patient_file/summary/rx_frameset.php
+++ interface/patient_file/summary/rx_frameset.php
@@ -4,13 +4,25 @@ include_once("../../globals.php");
 <html>
 <head>
 <?php html_header_show();?>
-<title><?php xl('Prescriptions','e'); ?></title>
+<title><?php echo xlt('Prescriptions'); ?></title>
+  <style>
+   .menu {
+      float:left;
+      width:20%;
+      height:80%;
+    }
+    .mainContent {
+      float:left;
+      width:75%;
+      height:80%;
+    }
+  </style>
 </head>
+<body>
 
-<frameset cols="18%,*" id="presc_frame">
- <frame src="rx_left.php" name="RxLeft" scrolling="auto">
- <frame src="<?php echo $GLOBALS['webroot'] ?>/controller.php?prescription&list&id=<?php echo $pid ?>"
-  name="RxRight" scrolling="auto">
-</frameset>
+ <iframe class="menu" src="rx_left.php" name="RxLeft" scrolling="auto"></iframe>
+ <iframe class="mainContent" src="<?php echo $GLOBALS['webroot'] ?>/controller.php?prescription&list&id=<?php echo $pid ?>"
+  name="RxRight" scrolling="auto"></iframe>
 
+</body>
 </html>
