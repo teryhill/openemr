71f746b7584b835158184da93fa064f9d80c8d52
teryhill <teryhill@yahoo.com>
2015-10-03 12:47:33 -0400

    Patient Ledger add in
    Starting of the addition of the ledger to demographics

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -586,6 +586,9 @@ if ($GLOBALS['patient_id_category_name']) {
 |
 <a href="stats_full.php?active=all" onclick='top.restoreSession()'>
 <?php echo htmlspecialchars(xl('Issues'),ENT_NOQUOTES); ?></a>
+|
+<a href="../../reports/patient_ledger.php?patient_id=<?php echo $pid;?>" onclick='top.restoreSession()'>
+<?php echo htmlspecialchars(xl('Ledger'),ENT_NOQUOTES); ?></a>
 
 <!-- DISPLAYING HOOKS STARTS HERE -->
 <?php
